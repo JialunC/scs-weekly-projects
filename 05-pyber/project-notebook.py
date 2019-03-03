f3ad207de282f065daeb57ceb75980229b9d917c
JialunC <jialun.tom.chen@gmail.com>
2019-03-03 14:34:01 -0500

    import data, project init

---
+++ 05-pyber/project-notebook.py
@@ -0,0 +1,14 @@
+
+# coding: utf-8
+
+# # Pyber Ride Sharing
+
+# In[1]:
+
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+# Dependencies and Setup
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
